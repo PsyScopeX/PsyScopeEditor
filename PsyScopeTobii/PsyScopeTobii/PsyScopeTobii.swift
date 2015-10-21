0b7a5e35b3948fc94da326e05519939e39f0deac
James <jamesa333uk@gmail.com>
2015-10-21 16:52:46 +0100

    various changes to setup for plugins

--- PsyScopeTobii/PsyScopeTobii/PsyScopeTobii.swift
+++ PsyScopeTobii/PsyScopeTobii/PsyScopeTobii.swift
@@ -10,27 +10,8 @@ import Foundation
 import PsyScopeFramework
 
 class PsyScopeTobii: NSObject, PSPluginInterface {
-    //each plugin can have multiple extensions (so you dont need a new bundle each time you develop an extension
-    
-    //	initializeClass: is called once when the plug-in is loaded. The plug-in's bundle is passed
-    //	as argument;
-    
-    static func initializeClass(theBundle: NSBundle!) -> Bool {
-        
-    }
-    
-    //	pluginsFor: returns strings of plugin names
-    static func pluginsFor(pluginTypeName: PSPluginType) -> [AnyObject]! {
-        
-    }
-    
-    // instantiatePlugin: returns an object (this is not possible currently in swift)
-    static func instantiatePlugin(pluginName: String!) -> AnyObject! {
-        
-    }
-    
-    //returns class of actual extension
-    static func getPSExtensionClass(pluginName: String!) -> AnyObject! {
-        
+   static func pluginsFor(pluginTypeName: PSPluginType) -> [NSObject.Type] {
+        Swift.print("Helllo")
+        return []
     }
 }
